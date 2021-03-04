955c059da5914dcda7394d9ef620e923de5eba16
RogueNPC <jordanbtansay@gmail.com>
2021-03-04 12:15:06 -0800

    [ADD] created User model and added comments

--- models.py
+++ models.py
@@ -2,7 +2,6 @@
 from tracker_app import db
 from sqlalchemy.orm import backref
 from flask_login import UserMixin
-import enum
 
 class Console(db.model):
     """Console model."""
@@ -11,8 +10,12 @@ class Console(db.model):
     company = db.Column(db.String(80))
     portable = db.Column(db.Boolean())
     console_notes = db.Column(db.String(200))
+
+    # The games - What games exist on this console?
     games = db.relationship('Game', back_populates='console')
 
+    # Who owns this console?
+    users_who_own = db.relationship('User', secondary='user_console', back_populates='consoles_owned')
 
 class Game(db.model):
     """Game model."""
@@ -21,17 +24,47 @@ class Game(db.model):
     publisher = db.Column(db.String(80))
     personal_rating = db.Column(db.Integer)
     game_notes = db.Column(db.String(200))
+
+    # The console - Which console do users play this game on?
     console_id = db.Column(db.Integer, db.ForeignKey('console.id'), nullable=False)
     console = db.relationship('Console', back_populates='games')
+
+    # The genres, e.g. first-person shooter (FPS), role-playing game (RPG), sandbox
     genres = db.relationship('Genre', secondary='game_genre', back_populates='games')
+
+    # Who owns this game?
+    users_who_own = db.relationship('User', secondary='user_game', back_populates='games_owned')
 
 class Genre(db.model):
     """Genre model."""
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False, unique=True)
+
+    # The games - what games have these genres?
     games = db.relationship('Game', secondary='game_genre', back_populates='genres')
 
+# Game <--> Genre table
 game_genre_table = db.Table('game_genre',
     db.Column('game_id', db.Integer, db.ForeignKey('game.id')),
     db.Column('genre_id', db.Integer, db.ForeignKey('genre.id'))
+)
+
+class User(UserMixin, db.Model):
+    """User model."""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+    consoles_owned = db.relationship('Console', secondary='user_console', back_populates='users_who_own')
+    games_owned = db.relationship('Game', secondary='user_game', back_populates='users_who_own')
+
+# Console <--> User table
+consoles_owned_table = db.Table('user_console',
+    db.Column('console_id', db.Integer, db.ForeignKey('console.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+# Game <--> User table
+games_owned_table = db.Table('user_game',
+    db.Column('game_id', db.Integer, db.ForeignKey('game.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
 )
